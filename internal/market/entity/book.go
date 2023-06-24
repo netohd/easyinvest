e7722f58bb0777727170aeb3032cbf4b5aaa9088
netohd <toninetocontato@gmail.com>
2023-06-23 22:00:49 -0300

    core definition

---
+++ internal/market/entity/book.go
@@ -0,0 +1,21 @@
+package entity
+
+import "sync"
+
+type Book struct {
+	Order         []*Order
+	Transaction   []*Transaction
+	OrdersChan    chan *Order
+	OrdersChanOut chan *Order
+	Wg            *sync.WaitGroup
+}
+
+func NewBook(orderChan chan *Order, orderChanOut chan *Order, wg *sync.WaitGroup) *Book {
+	return &Book{
+		Order:         []*Order{},
+		Transaction:   []*Transaction{},
+		OrdersChan:    orderChan,
+		OrdersChanOut: orderChanOut,
+		Wg:            wg,
+	}
+}

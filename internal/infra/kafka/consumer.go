06808e96aad0a2d3dcb49d3832b88a2c1596ec92
netohd <toninetocontato@gmail.com>
2023-06-25 14:00:39 -0300

    kafka producer and consumer changes

--- internal/infra/kafka/consumer.go
+++ internal/infra/kafka/consumer.go
@@ -6,3 +6,28 @@ type Consumer struct {
 	ConfigMap *ckafka.ConfigMap
 	Topics    []string
 }
+
+func NewConsumer(configMap *ckafka.ConfigMap, topics []string) *Consumer {
+	return &Consumer{
+		ConfigMap: configMap,
+		Topics:    topics,
+	}
+}
+
+func (c *Consumer) Consume(msgChan chan *ckafka.Message) error {
+	consumer, err := ckafka.NewConsumer(c.ConfigMap)
+	if err != nil {
+		panic(err)
+	}
+	err = consumer.SubscribeTopics(c.Topics, nil)
+	if err != nil {
+		panic(err)
+	}
+	// For infinito, quando chega msg joga para esse canal
+	for {
+		msg, err := consumer.ReadMessage(-1)
+		if err == nil {
+			msgChan <- msg
+		}
+	}
+}

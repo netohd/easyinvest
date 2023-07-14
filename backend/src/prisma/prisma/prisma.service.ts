b6b6351a74e690f1dd82d5f7e049c71f71070fe1
netohd <toninetocontato@gmail.com>
2023-07-13 23:59:41 -0300

    permissions change

--- backend/src/prisma/prisma/prisma.service.ts
+++ backend/src/prisma/prisma/prisma.service.ts
@@ -7,9 +7,9 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     await this.$connect();
   }
 
-  async enableShutdownHooks(app: any) {
-    this.$on('beforeExit', async () => {
-      await app.close();
-    });
-  }
+  // async enableShutdownHooks(app: any) {
+  //   this.$on('beforeExit', async () => {
+  //     await app.close();
+  //   });
+  // }
 }
